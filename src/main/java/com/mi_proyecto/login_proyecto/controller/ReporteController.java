64df50f16acbd4c5fdc75cc1c246e8de0a68ddb7
cdelriot1121 <carandresdelrio1121@gmail.com>
2024-10-30 16:26:34 -0500

    modificaciones para que los trabajadores creen reportes

--- src/main/java/com/mi_proyecto/login_proyecto/controller/ReporteController.java
+++
@@ -1,9 +0,0 @@
-package com.mi_proyecto.login_proyecto.controller;
-
-import org.springframework.stereotype.Controller;
-
-
-@Controller
-public class ReporteController {
-
-}
--- src/main/java/com/mi_proyecto/login_proyecto/model/Reporte.java
+++ src/main/java/com/mi_proyecto/login_proyecto/model/Reporte.java
@@ -1,8 +1,24 @@
 package com.mi_proyecto.login_proyecto.model;
 
-import jakarta.persistence.*;
+
+import io.micrometer.common.lang.NonNull;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "Reportes")
 public class Reporte {
@@ -11,6 +27,12 @@ public class Reporte {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
 
-    
+    @Basic
+    @NonNull
+    private String contenido;
+
 }
